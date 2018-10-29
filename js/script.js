ee9c39d74bd43b8afc18570abc9430ee968245ab
Tim Hemendinger <themendinger@gmail.com>
2018-10-28 20:24:41 -0500

    Dynamic screens for each house

--- js/script.js
+++ js/script.js
@@ -1,34 +1,50 @@
 $(function() {
 
+    $('.crest-image').hide();
+
     playAudio('audio/hp-theme.mp3', true);
 
     var randomNum = 0;
     var house = '';
 
     $('#getHouseBtn').on('click', () => {
+        $('.intro-screen').fadeOut('slow', () => {
+            $('.house-result').fadeIn('slow');
+        });
+
         randomNum = genRandomInt(4);
         house = getHouse(randomNum);
-        console.log(house);
+
     });
     
 });
 
+function generateHouse(house) {
+    
+    capitalizedName = house.charAt(0).toUpperCase() + house.slice(1);
+
+    $('.g-text').text(capitalizedName);
+    $('#' + house).show();
+    $('.house-header').addClass(house + '-text');
+    $('.house-body').addClass(house + '-bg');
+}
+
 function getHouse(randomNum) {
     switch (randomNum) {
         case 0:
-            return 'Gryffindor';
+            generateHouse('gryffindor');
             break;
 
         case 1:
-            return 'Hufflepuff';
+            generateHouse('hufflepuff');
             break;
 
         case 2:
-            return 'Ravenclaw';
+            generateHouse('ravenclaw');
             break;
 
         case 3:
-            return 'Slytherin';
+            generateHouse('slytherin');
             break;
     }
 }
@@ -41,7 +57,7 @@ function playAudio (audioPath, loop) {
     if (audio) {
         audio.stop();
     }
-    
+
     var audio = new Audio(audioPath);
     audio.loop = loop;
     audio.play();
